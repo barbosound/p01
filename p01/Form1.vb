2d39434c46d958d0b9ea2b387048881801ea46b5
pau <barbo_94@hotmail.com>
2016-10-14 16:46:44 -0500

    primer punt

--- p01/Form1.vb
+++ p01/Form1.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Form1
+    Public inici As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        inici = "Inici coodrdenades X" + CStr(e.X) + " Y " + CStr(e.Y)
+        Me.Text = inici
+
+        If 
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        Me.Text = inici + " Final coodrdenades X" + CStr(e.X) + " Y " + CStr(e.Y)
+    End Sub
 End Class
