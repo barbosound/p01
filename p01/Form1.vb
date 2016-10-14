0bcb90aac4a7a7cbd8119cd25e10122309575656
pau <barbo_94@hotmail.com>
2016-10-14 17:12:31 -0500

    segon punt

--- p01/Form1.vb
+++ p01/Form1.vb
@@ -1,17 +1,26 @@
 ﻿Public Class Form1
     Public inici As String
+    Public final As String
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
     Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
         inici = "Inici coodrdenades X" + CStr(e.X) + " Y " + CStr(e.Y)
+
         Me.Text = inici
 
-        If  
+
     End Sub
 
     Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
-        Me.Text = inici + " Final coodrdenades X" + CStr(e.X) + " Y " + CStr(e.Y)
+        final = inici + " Final coodrdenades X" + CStr(e.X) + " Y " + CStr(e.Y)
+
+        If e.Button = MouseButtons.Right Then
+            Me.Text = final + " BOTÓ DRET "
+        ElseIf e.Button = MouseButtons.Left Then
+            Me.Text = final + " BOTÓ ESQUERRE "
+        End If
+
     End Sub
 End Class
